df737eb0879c569073a449de2f0619a5d30ffe9e
kevinboyd76 <kevinboyd76@gmail.com>
2022-06-05 00:15:49 -0500

    Add the knn regression model

--- Boyd_Pandey_Project1.R
+++ Boyd_Pandey_Project1.R
@@ -52,11 +52,16 @@ ggplot(lifeExpectancy, aes(x=Status, y=Life.expectancy, fill = Status)) +
   ggtitle("Distribution of Life Expectancy for Developed and Developing Countries") +
   theme_classic()
 
+
 ggplot() +
-  geom_density(data = lifeExpectancy, aes(x=HIV.AIDS), fill = "darkorange", alpha = 0.5) +
-  geom_density(data = lifeExpectancy, aes(x=Measles), fill = "darkblue", alpha = 0.5) +
-  geom_density(data = lifeExpectancy, aes(x=Diphtheria), fill = "steelblue", alpha = 0.5) +
+  geom_density(data = lifeExpectancy, aes(x=HIV.AIDS), fill = "darkorange",color = "darkorange", alpha = 0.5, size = 1) +
+  geom_density(data = lifeExpectancy, aes(x=Measles), fill = "darkblue", color = "darkblue", alpha = 0.5, size = 1) +
+  geom_density(data = lifeExpectancy, aes(x=Polio), fill = "green",color = "green", alpha = 0.5, size = 1) +
+  geom_density(data = lifeExpectancy, aes(x=Diphtheria), fill = "steelblue", color = "steelblue", alpha = 0.5, size = 1) +
   xlim(0,10) +
+  labs(title = "HIV/AIDS, Measles, Diptheria, and Polio Distributions",
+       caption = " HIV/AIDS = Orange, Measles = Dark Blue, Diptheria = Light Blue, Polio = Green") +
+  xlab("Diseases") +
   theme_classic()
 
 
@@ -87,7 +92,7 @@ lifeExpectancy <- lifeExpectancy[,-16] # Removing thinness.1.19
 VIFmodel <- lm(Life.expectancy~.,data=lifeExpectancy)
 vif(VIFmodel) # All VIFs way under 10 now
 
-#lifeExpectancy <- lifeExpectancy[,c(-1,-6,-17,-19)]
+lifeExpectancy <- lifeExpectancy[,c(-1,-6,-17,-19)]
 
 
 # Check summary statistics
@@ -95,7 +100,7 @@ summary(VIFmodel)
 
 
 # Objective 1: Build regression models and identify key relationships and observe those relationships.
-
+set.seed(5)
 splitPerc = .85
 trainIndices = sample(1:dim(lifeExpectancy)[1],round(splitPerc * dim(lifeExpectancy)[1]))
 train = lifeExpectancy[trainIndices,]
@@ -114,7 +119,8 @@ plot(m3)
 # Model from Stepwise feature selection
 
 #model2 <- lm(Life.expectancy~Adult.Mortality+Income.composition.of.resources+Schooling+HIV.AIDS+Diphtheria+percentage.expenditure+BMI+Polio+Status+under.five.deaths,data=train)
-model2 <- lm(Life.expectancy~Adult.Mortality+Income.composition.of.resources+Schooling+HIV.AIDS+Diphtheria+percentage.expenditure+BMI+Polio+Status+Measles,data=train) # Added by Shikha
+#model2 <- lm(Life.expectancy~Adult.Mortality+Income.composition.of.resources+Schooling+HIV.AIDS+Diphtheria+percentage.expenditure+BMI+Polio+under.five.deaths+Status+Alcohol,data=train) # Added by Shikha
+model2 <- lm(Life.expectancy~Adult.Mortality+Income.composition.of.resources+Schooling+HIV.AIDS+Diphtheria+percentage.expenditure+BMI+Polio+Measles+Year+Status,data=train)
 summary(model2)
 plot(model2)
 vif(model2)
@@ -130,6 +136,7 @@ test$Life.expectancy2 <- preds[,1]
 preds2 <- test[,c(3,19)]
 head(preds2)
 
+# plot the observed and predicted
 ggplot() +
   geom_density(data = preds2, aes(x=Life.expectancy)) +
   geom_density(data = preds2, aes(x=Life.expectancy2), color = "Red") +
@@ -139,10 +146,63 @@ ggplot() +
 
 
 
+################################################################################
+################################################################################
+###########################      Objective 2.1     #############################
+################################################################################
+################################################################################
+set.seed(5)
+splitPerc = .85
+trainIndices = sample(1:dim(lifeExpectancy)[1],round(splitPerc * dim(lifeExpectancy)[1]))
+train = lifeExpectancy[trainIndices,]
+test = lifeExpectancy[-trainIndices,]
+
+# Adult Mortality
+ggplot() +
+  geom_density(data = lifeExpectancy, aes(x=sqrt(Adult.Mortality)), fill = "darkorange", alpha = 0.5)
+ggplot() +
+  geom_density(data = lifeExpectancy, aes(x=Adult.Mortality), fill = "darkorange", alpha = 0.5)
+
+# Income Composition of Resources
+ggplot() +
+  geom_density(data = lifeExpectancy, aes(x=(Income.composition.of.resources^2)), fill = "steelblue", alpha = 0.5)
+ggplot() +
+  geom_density(data = lifeExpectancy, aes(x=Income.composition.of.resources), fill = "steelblue", alpha = 0.5)
+
+# Schooling
+ggplot() +
+  geom_density(data = lifeExpectancy, aes(x=(Schooling)), fill = "darkblue", alpha = 0.5)
+
+
+# run complex model
+model2 <- lm(Life.expectancy~sqrt(Adult.Mortality)*(Income.composition.of.resources^2)*percentage.expenditure*Schooling*BMI+HIV.AIDS*Measles+Diphtheria+Polio+Year*Status,data=train)
+summary(model2)
+plot(model2)
+vif(model2)
+
+# model metrics
+hist(model2$residuals, col = "darkslateblue", main = "Histogram of Residuals")
+sqrt(sum((model2$residuals)^2)) # Just the comment added by Shikha - value for this is 189.1191
+
+# predictions
+preds <- predict(model2, newdata = test, interval = "prediction")
+preds <- as.data.frame(preds)
+test$Life.expectancy2 <- preds[,1]
+preds2 <- test[,c(3,19)]
+head(preds2)
+
+# plot the observed and predicted
+ggplot() +
+  geom_density(data = preds2, aes(x=Life.expectancy)) +
+  geom_density(data = preds2, aes(x=Life.expectancy2), color = "Red") +
+  ggtitle("Predictions vs Actual Values") +
+  xlab("Life Expectancy") +
+  theme_classic()
+
 
 ################################################################################
 library(leaps)
-reg.fwd <- regsubsets(Life.expectancy~.,data = train, method = "exhaustive", nvmax = 18)
+reg.fwd <- regsubsets(Life.expectancy~.,data = train, method = "forward", nvmax = 18)
 coef(reg.fwd,11)
 
 # summary statistics from forward model
@@ -186,4 +246,81 @@ points(index,testASE[index],col="red",pch=10)
 rss<-summary(reg.fwd)$rss
 lines(1:20,rss/257,lty=3,col="blue")  #Dividing by 100 since ASE=RSS/sample size
 
+################################################################################
+
+lifeExpectancy <- read.csv("Life Expectancy Data.csv", header = T)
+lifeExpectancy <- lifeExpectancy[,-c(1,3)]                                      # remove catigorical variables -         MSE:  6.261776 MAE:  1.830244  RMSE:  2.502354
+lifeExpectancy <- lifeExpectancy[,-c(1,3,6,7,9,12,14,17,18,19,20)]              # linear regression model variables -    MSE:  7.812284 MAE:  2.028182  RMSE:  2.795046
+lifeExpectancy <- lifeExpectancy[,c(-1,-3,-6,-17,-19)]                          # remove high VIF variables -            MSE:  9.299161 MAE:  2.163902  RMSE:  3.049453
+library(caret)
+lifeExpectancy <-  na.omit(lifeExpectancy)
+
+set.seed(12)
+
+# make the train and test sets
+indexes = createDataPartition(lifeExpectancy$Life.expectancy, p = .85, list = F)
+train = lifeExpectancy[indexes, ]
+test = lifeExpectancy[-indexes, ]
+
+train_x = train[, -2]
+train_x = scale(train_x)[,]
+train_y = train[,2]
+
+test_x = test[, -2]
+test_x = scale(test[,-2])[,]
+test_y = test[,2]
+
+# knn reg model 
+knnmodel = knnreg(train_x, train_y)
+
+str(knnmodel)
+
+pred_y = predict(knnmodel, data.frame(test_x))
+
+
+# Check assumptions
+print(data.frame(test_y, pred_y))
+
+mse = mean((test_y - pred_y)^2)
+mae = caret::MAE(test_y, pred_y)
+rmse = caret::RMSE(test_y, pred_y)
+
+cat("MSE: ", mse, "MAE: ", mae, " RMSE: ", rmse)
+
+
+
+# plot knn regression
+x = 1:length(test_y)
+
+plot(x, test_y, col = "red", type = "l", lwd=2,
+     main = "Life Expectancy Test Data Prediction")
+lines(x, pred_y, col = "blue", lwd=2)
+legend("topright",  legend = c("original-lifeExpectancy", "predicted-lifeExpectancy"), 
+       fill = c("red", "blue"), col = 2:3,  adj = c(0, 0.6))
+grid()
+
+
+
+################################################################################
+
+model = knnreg(Life.expectancy ~ ., data = lifeExpectancy)
+plot(model)
+str(model)
+
+
+set.seed(1)
+
+tuneGrid <- expand.grid(
+  k = seq(5, 9, by = 1)
+)
+
+model5 <- train(
+  medv ~ .,
+  data = training,
+  method = 'knn',
+  preProcess = c("center", "scale"),
+  trControl = ctrl,
+  tuneGrid = tuneGrid
+)
+model5
 
